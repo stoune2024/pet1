f26775d1b58cde100771295e8645728594ed509d
Maxim <mchubaroff@yandex.ru>
2025-01-08 00:45:44 +0500

    Сделал раздел сайта (про Марсика) с использованием шаблонирования (динамические html-страницы).

--- app/routers/pages.py
+++ app/routers/pages.py
@@ -15,3 +15,15 @@ async def get_index(request: Request):
 @router.get('/barsik', response_class=HTMLResponse)
 async def get_barsik_page(request: Request):
     return templates.TemplateResponse(request=request, name="barsik.html")
+
+@router.get('/marsik', response_class=HTMLResponse)
+async def get_marsik_page(request: Request):
+    return templates.TemplateResponse(request=request, name="marsik.html")
+
+@router.get('/bonus', response_class=HTMLResponse)
+async def get_bonus_page(request: Request):
+    return templates.TemplateResponse(request=request, name="bonus.html")
+
+@router.get('/oauth', response_class=HTMLResponse)
+async def get_oauth_page(request: Request):
+    return templates.TemplateResponse(request=request, name="oauth.html")
