6ef728f878376517704d58c5626df74f76d9ec4e
Maxim <mchubaroff@yandex.ru>
2025-02-24 21:42:55 +0500

    Тестирование официально закончено. Далее: добавление функционала

--- tests/test_safety.py
+++ tests/test_safety.py
@@ -2,10 +2,9 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import create_engine, StaticPool, SQLModel, Session
 from app.main import app, OAuth2PasswordRequestForm, authenticate_user, UserCreate, pwd_context, User, \
-    get_safety_session
+    get_safety_session, verify_token, TokenData
 from typing import Annotated
 from fastapi import Form
-
 
 @pytest.fixture(name="session")
 def session_fixture():
@@ -43,6 +42,15 @@ def create_user_fixture():
     user_mapped = User.model_validate(user, update=user_extra_data)
     return user_mapped
 
+@pytest.fixture(name='token')
+def token_fixture():
+    def override_verify_token():
+        fake_token = TokenData(username='fake_username')
+        return fake_token
+    app.dependency_overrides[verify_token] = override_verify_token
+    yield override_verify_token()
+    app.dependency_overrides.clear()
+
 
 def test_login_for_access_token(session: Session, client: TestClient, create_user: User):
     session.add(create_user)
@@ -60,14 +68,20 @@ def test_login_for_access_token_user_not_found(session: Session, client: TestCli
     response = client.post("/token", data={"username": "unknown_user", "password": "unknown_password"})
     assert response.status_code == 404
     assert response.headers['WWW-Authenticate'] == "Bearer"
+    assert '<!doctype html>' in response.text
 
 
-def test_validate_login_form(session: Session, client: TestClient, create_user: User):
+def test_validate_login_form(session: Session, client: TestClient, create_user: User, token: TokenData):
     session.add(create_user)
     session.commit()
     response = client.post('/login', data={"username": "fake_user", "password": "fake_password"})
-    # assert response.status_code == 401
-    assert response.status_code == 303
-    # assert response.headers['WWW-Authenticate'] == "Bearer"
-    # assert '<!doctype html>' in response.text
+    assert response.status_code == 200
+    assert '<!doctype html>' in response.text
 
+def test_validate_login_form_user_not_found(session: Session, client: TestClient, create_user: User, token: TokenData):
+    session.add(create_user)
+    session.commit()
+    response = client.post('/login', data={"username": "unknown_user", "password": "unknown_password"})
+    assert response.status_code == 404
+    assert '<!doctype html>' in response.text
+    assert response.headers['WWW-Authenticate'] == "Bearer"
